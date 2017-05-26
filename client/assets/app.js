ba9657ed4775c8517b5246b9d963355fd5f0c200
Harris Gomez <harrisgomez28@gmail.com>
2017-05-26 14:45:18 -0400

    second commit

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,9 +1,9 @@
-var app = angular.module('calcApp', ['ngRoute']);
-
-app.config(function($routeProvider){
-    $routeProvider
-    .when('/', {
-        templateUrl: 'static/partials/calculator.html',
-        controller: 'calculatorController'
-    })
-})
+// var app = angular.module('calcApp', []);
+//
+// app.config(function($routeProvider){
+//     $routeProvider
+//     .when('/', {
+//         templateUrl: 'static/partials/calculator.html',
+//         controller: 'calculatorController'
+//     })
+// })
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var bodyParser = require('body-parser');
 var port = 8000;
 
 app.use(express.static(path.join(__dirname, './client'))); // loads your main index. Basically says look inside this folder and run what's inside
-app.use(express.static(path.join(__dirname, './bower_components'))); // ngRoute no longer part of angular.js file, so use bower
-app.use(bodyParser.urlencoded({ extended: true }));
 
 var server = app.listen(port, function() {
     console.log("listening on port 8000");
