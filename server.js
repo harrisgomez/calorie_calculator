0844cbedbafebb15f723fa05e9c0e80ce772d2dd
Harris Gomez <harrisgomez28@gmail.com>
2017-05-30 12:44:41 -0400

    fifth commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var port = 8000;
 
 app.use(express.static(path.join(__dirname, './client'))); // loads your main index. Basically says look inside this folder and run what's inside
+app.use(express.static(path.join(__dirname, './test'))); // loads your main index. Basically says look inside this folder and run what's inside
 
 var server = app.listen(port, function() {
     console.log("listening on port 8000");
